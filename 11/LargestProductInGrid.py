7b659192feb1941a11808d755e925c43ff36d3a0
Tim Hillier <Tim.r.hillier@gmail.com>
2018-07-18 18:04:37 -0700

    11`
    working on 11

--- 11/LargestProductInGrid.py
+++ 11/LargestProductInGrid.py
@@ -5,6 +5,7 @@ largest = 0
 with open("numbers.txt") as f:
     content = [line.split() for line in f]
 
+
 numbers =[[int(y) for y in x] for x in content]
 
 
@@ -14,21 +15,38 @@ for a in range(0,len(numbers)):
         s = 1
         for y in range(0,search):
             s = s * numbers[a][x+y]
+        if(s > largest):
+            largest = s
+        s = 1
+#do diagonal to right
+    for x in range(0,len(numbers[0])):
+        s = 1
+        for y in range(0,search):
+            if(a+search < len(numbers[0]) and y + search < len(numbers[0])):
+                s = s * numbers[a+search][y+search]
+        if(s > largest):
+            largest = s
+        s = 1
+
+# do diagonal to left
+    for x in range(0,len(numbers[0])):
+        s = 1
+        for y in range(0,search):
+            if(a + search < len(numbers[0]) and y - search > 0):
+                s = s * numbers[a+search][y-search]
             if(s > largest):
                 largest = s
             s = 1
 
-
-
-for a in range(0,len(numbers)-search):
-    s = 1
-    #sum of left, right
-    for x in range(0,search):
-        s = s * numbers[a+x]
-        
-        if(s>largest):
-            largest = s
+# do down
+    for x in range(0,len(numbers[0])):
         s = 1
+        for y in range(0,search):
+            if(a > len(numbers[0]) + search):
+                s = s * numbers[a][y + search]
+            if(s > largest):
+                largest = s
+            s = 1
 
 print(largest)
 
