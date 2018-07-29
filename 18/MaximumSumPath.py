38f2cec00ffdfdf9a542c982fc3cbd43890b018f
Tim Hillier <Tim.r.hillier@gmail.com>
2018-07-28 17:30:59 -0700

    18,67 complete

--- 18/MaximumSumPath.py
+++ 18/MaximumSumPath.py
@@ -4,35 +4,27 @@ with open("triangle.txt") as f:
     content = [line.split() for line in f]
 
 numbers =[[int(y) for y in x] for x in content]
+with open("67Triangle.txt") as f:
+    content = [line.split() for line in f]
+numbers2 = [[int(y) for y in x] for x in content]
 
-def solve(num):
-    lastPos = 0
-    cost = 0
-    path = [numbers[0][0]]
-    for i in range(1,len(num)):
-        print("comparing: " + str(num[i][lastPos]) + "," + str(num[i][lastPos+1]))
-        if(num[i][lastPos] >= num[i][lastPos+1]):
-            cost += num[i][lastPos]
-            lastPos = lastPos
-            path.append(num[i][lastPos])
-            print("WINNER: " + str(num[i][lastPos]) + " at Position: " + str(lastPos))
-        elif(num[i][lastPos+1] > num[i][lastPos]):
-            cost += num[i][lastPos+1]
-            lastPos = lastPos + 1
-            path.append(num[i][lastPos])
-            print("WINNER: " + str(num[i][lastPos]) + " at Position: " + str(lastPos))
-
-    print(sum(path))
-    print(path)
+def solve(tri):
+    m = n = len(tri) - 1
+    for i in range(m-1,-1,-1):
+        for j in range(i+1):
+            if(tri[i+1][j]>tri[i+1][j+1]):
+                tri[i][j]+=tri[i+1][j]
+            else:
+                tri[i][j]+=tri[i+1][j+1]
+    return tri[0][0]
+print("Problem 18")
+print(solve(numbers))
 
 
-def Dijkstra(Graph,Source):
-    visited = []
-    
+#this is number 67
+#https://projecteuler.net/problem=67
+print("Problem 67")
 
 
 
-
-
-
-print(numbers)
+print(solve(numbers2))
